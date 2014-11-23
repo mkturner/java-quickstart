fe57efd63920cb321bf301d09f12a566a86366dd
Marvin K. Turner <contact@marvinkturner.com>
2014-11-23 00:04:48 -0500

    Adds "Hello World"
    Adds println() that displays Hello World
    in the console because One does not simply
    learn a new language with doing a Hello
    World program.

--- LearnJava/src/org/example/learn/Demo.java
+++ LearnJava/src/org/example/learn/Demo.java
@@ -8,7 +8,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my test message");
-
+		System.out.println("Hello World");
 	}
 
 }
