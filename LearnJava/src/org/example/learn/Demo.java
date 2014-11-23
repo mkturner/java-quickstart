f4bf946143dc7c75eb66456e25512a21b6347974
Marvin K. Turner <contact@marvinkturner.com>
2014-11-23 00:13:25 -0500

    Adds comments + intro to variables

--- LearnJava/src/org/example/learn/Demo.java
+++ LearnJava/src/org/example/learn/Demo.java
@@ -7,8 +7,15 @@ public class Demo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//making variables
+		String testMessage = "This in asnother test.";
+		
+		//printing to console window
 		System.out.println("This is my test message");
 		System.out.println("Hello World");
+		
+		//printing using variables
+		System.out.println(testMessage);
 	}
 
 }
