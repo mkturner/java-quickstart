2ae8e8befc13d38629ab7e18f3122988351e5e6b
Marvin K. Turner <contact@marvinkturner.com>
2014-11-25 13:01:49 -0500

    Adds new class file for testing conditionals.
    Moving on in the JAVA tutorial, I am now
    going over the syntax of if-else statements.
    while & do-while are expected to follow 
    hereafter.

---
+++ LearnJava/src/org/example/learn/Demo2.java
@@ -0,0 +1,22 @@
+package org.example.learn;
+
+public class Demo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//first we will test conditionals..
+		int lives = 3;
+		
+		if (lives < 0){
+			System.out.println("Game Over!");
+		}
+		else{
+			System.out.println("You are still alive!");
+		}
+	}
+
+}
