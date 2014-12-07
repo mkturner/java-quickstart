671010358fae628737a8683fb65aa5574c24d19f
Marvin K. Turner <contact@marvinkturner.com>
2014-12-07 01:31:50 -0500

    Gives methods/members to IventoryItem class

--- LearnJava/src/org/example/learn/InventoryItem.java
+++ LearnJava/src/org/example/learn/InventoryItem.java
@@ -1,7 +1,33 @@
 package org.example.learn;
 
 enum ItemType {POTION, RING, ARMOR}
-//adds enum for
+//adds enumeration to specify all the possible item types
 public class InventoryItem {
+	
+	private ItemType type;
+	private String name;
+	
+	public InventoryItem(ItemType type, String name) {
+		super();
+		this.type = type;
+		this.name = name;
+	}
 
+	public ItemType getType() {
+		return type;
+	}
+
+	public void setType(ItemType type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
