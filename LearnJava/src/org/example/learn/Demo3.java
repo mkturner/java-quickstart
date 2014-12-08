d9f745ed5e73b491e04136c15a4829c5f1a56a07
Marvin K. Turner <contact@marvinkturner.com>
2014-12-07 23:42:41 -0500

    Deletes objects
    walks through how do drop objects from 
    inventory and what happens when you try 
    to delete something that is not in the
    inventory.

--- LearnJava/src/org/example/learn/Demo3.java
+++ LearnJava/src/org/example/learn/Demo3.java
@@ -33,6 +33,9 @@ public class Demo3 {
 		bawse.addInventoryItem(new InventoryItem(ItemType.POTION, "MMG Juice"));
 		bawse.addInventoryItem(new InventoryItem(ItemType.ARMOR, "MMG Pendant"));
 		
+		InventoryItem endorsementDeal = new InventoryItem(ItemType.PAPERWORK, "Reebok Deal");
+		bawse.addInventoryItem(endorsementDeal);  
+		
 		//Show all items belonging to bawse object
 		//aggregate all items into allItems
 		ArrayList<InventoryItem> allItems = bawse.getInventoryItems();
@@ -43,8 +46,24 @@ public class Demo3 {
 			//separate from InventoryItem class 
 		}
 		
+		//making space
+		System.out.println();
 		//lets delete items
-		//one that obviously doesn't exist
-		boolean wasDeletedFalse = bawse.dropInvetoryItem()
+		//one that obviously doesn't exist in the inventory of bawse
+		//create item so that it is valid in program
+		InventoryItem CTEDeal = new InventoryItem(ItemType.PAPERWORK, "CTE Record Deal");
+		System.out.println("Let's Delete Bawse's CTE Contract.");
+		System.out.println("Deleting...");
+		boolean wasDeletedFalse = bawse.dropInvetoryItem(CTEDeal);
+		System.out.println("Was the CTE Contract Dropped?");
+		System.out.println(wasDeletedFalse);
+		//Why false? The item existed, but it was not
+		//a part of the inventory belonging to the
+		//bawse object. 
+		System.out.println("Lets delete something that actually is in the inventory.");
+		System.out.println("Remember when he lost his Reebok deal? lmao.");
+		boolean wasDeletedTrue = bawse.dropInvetoryItem(endorsementDeal);
+		System.out.println("Was the Reebok Deal Dropped?");
+		System.out.println(wasDeletedTrue);
 	}	
 }
